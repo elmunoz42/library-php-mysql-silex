1dbaee0a66c082a7a7cd8534225793dc74eb137b
Philip Putnam <putnamp989@gmail.com>
2017-03-01 13:29:33 -0800

    add get, set, save, getAll, and deleteAll functions to Book.php with passing tests

---
+++ src/Book.php
@@ -0,0 +1,55 @@
+<?php
+    class Book
+    {
+        private $title;
+        private $id;
+
+        function __construct($title, $id = null)
+        {
+            $this->title = $title;
+            $this->id = $id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getTitle()
+        {
+            return $this->title;
+        }
+
+        function setTitle($new_title)
+        {
+            $this->title = $new_title;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO books (title) VALUES ('{$this->getTitle()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_books = $GLOBALS['DB']->query("SELECT * FROM books;");
+            $books = [];
+
+            foreach($returned_books as $book)
+            {
+                $title = $book['title'];
+                $id = $book['id'];
+                $new_book = new Book($title, $id);
+                array_push($books, $new_book);
+            }
+            return $books;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM books;");
+        }
+    }
+
+?>
---
+++ tests/BookTest.php
@@ -0,0 +1,124 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    $server = 'mysql:host=localhost:8889;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    require_once 'src/Book.php';
+
+
+    class BookTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Book::deleteAll();
+        }
+
+        function test_getId()
+        {
+            //Arrange
+            $title = 'A Game of Thrones';
+            $id = 32;
+            $new_book = new Book($title, $id);
+
+            //Act
+            $result = $new_book->getId();
+
+            //Assert
+            $this->assertEquals(32, $result);
+
+        }
+
+        function test_getTitle()
+        {
+            //Arrange
+            $title = 'A Game of Thrones';
+            $new_book = new Book($title);
+
+            //Act
+            $result = $new_book->getTitle();
+
+            //Assert
+            $this->assertEquals('A Game of Thrones', $result);
+        }
+
+        function test_setTitle()
+        {
+            //Arrange
+            $title = 'A Game of Thrones';
+            $new_title = 'AGoT';
+            $new_book = new Book($title);
+
+            //Act
+            $new_book->setTitle($new_title);
+            $result = $new_book->getTitle();
+
+            //Assert
+            $this->assertEquals('AGoT', $result);
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $title = 'A Game of Thrones';
+            $new_book = new Book($title);
+            $new_book->save();
+
+            //Act
+            $result = Book::getAll();
+
+            //Assert
+            $this->assertEquals([$new_book], $result);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $title = 'A Game of Thrones';
+            $new_book = new Book($title);
+            $new_book->save();
+
+            $title2 = 'A Feast of Crows';
+            $new_book2 = new Book($title2);
+            $new_book2->save();
+
+            $title3 = 'The Name of the Wind';
+            $new_book3 = new Book($title3);
+            $new_book3->save();
+
+            //Act
+            $result = Book::getAll();
+
+            //Assert
+            $this->assertEquals([$new_book, $new_book2, $new_book3], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $title = 'A Game of Thrones';
+            $new_book = new Book($title);
+            $new_book->save();
+
+            $title2 = 'A Feast of Crows';
+            $new_book2 = new Book($title2);
+            $new_book2->save();
+
+            $title3 = 'The Name of the Wind';
+            $new_book3 = new Book($title3);
+            $new_book3->save();
+
+            //Act
+            Book::deleteAll();
+            $result = Book::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+    }
+?>
