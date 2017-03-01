17d12c41245c79c33f0c19008686c2d96cb84f2c
Philip Putnam <putnamp989@gmail.com>
2017-03-01 11:31:59 -0800

    basic template with new .htaccess file working in web directory

---
+++ app/app.php
@@ -0,0 +1,17 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get('/', function() use($app) {
+
+        return $app['twig']->render('index.html.twig');
+    });
+
+    return $app;
+?>
