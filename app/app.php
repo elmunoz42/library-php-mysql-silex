bffa2172e4406ac6723f894b5fa33114a8e9f8ab
Philip Putnam <putnamp989@gmail.com>
2017-03-02 07:56:15 -0800

    add simple form for librarian user to add book

--- app/app.php
+++ app/app.php
@@ -1,6 +1,20 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Author.php';
+    require_once __DIR__.'/../src/Book.php';
+    require_once __DIR__.'/../src/Copies.php';
+    require_once __DIR__.'/../src/Patron.php';
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    $server = 'mysql:host=localhost:8889;dbname=library';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
     $app = new Silex\Application();
 
@@ -11,6 +25,12 @@
     $app->get('/', function() use($app) {
 
         return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get('/main', function() use($app) {
+        $books = Book::getAll();
+
+        return $app['twig']->render('main.html.twig');
     });
 
     return $app;
